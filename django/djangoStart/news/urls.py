ba6d12acc992fd808c76f22e94c3916beff037bd
beGG1 <70636894+beGG1@users.noreply.github.com>
2022-03-03 17:20:51 +0300

    first_steps

---
+++ django/djangoStart/news/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    path('', views.news_home, name='news_home'),
+    path('<int:pk>', views.NewsDetailView.as_view(), name='news_detail')
+]
